de41c32b643b8c3e2300e66ced6174b1b63d4dbd
Kevin J. Lynagh <kevin@keminglabs.com>
2013-04-07 12:15:35 -0700

    ClojureScript project.clj doesn't need to reference lein-cljsbuild.

--- cljs_package/project.clj
+++ cljs_package/project.clj
@@ -1,10 +1,5 @@
 (defproject com.keminglabs/sliced-bananas "0.1.0-SNAPSHOT"
   :description "JSON tagged literal system"
   :license {:name "BSD" :url "http://www.opensource.org/licenses/BSD-3-Clause"}
-
   :min-lein-version "2.0.0"
-  :plugins [[lein-cljsbuild "0.3.0"]]
-
-  :source-paths ["src/cljs"]
-
   :resource-paths ["pkg"])
