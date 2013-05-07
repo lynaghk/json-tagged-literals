5510c23705685fb4b007fb1933b2f2efaa679d62
Kevin J. Lynagh <kevin@keminglabs.com>
2013-05-06 21:47:21 -0700

    Release 0.1.0.

--- cljs_package/project.clj
+++ cljs_package/project.clj
@@ -1,4 +1,4 @@
-(defproject com.keminglabs/sliced-bananas "0.1.0-SNAPSHOT"
+(defproject com.keminglabs/sliced-bananas "0.1.0"
   :description "JSON tagged literal system"
   :license {:name "BSD" :url "http://www.opensource.org/licenses/BSD-3-Clause"}
   :min-lein-version "2.0.0"
